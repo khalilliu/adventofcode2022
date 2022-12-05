1fc46850045b1c6a296e4ac76bbb9cfac81a9599
khalilliu <lkhaliliu@gmail.com>
2022-12-05 23:23:54 +0800

    chore: finish day 05

---
+++ 05/p1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+vector<string> crates;
+vector<array<int, 3>> ops;
+
+int main() {
+  string s;
+  bool is_op = false;
+  while(getline(cin, s)) {
+    if(s.empty()) is_op = true;
+    else {
+      if(!is_op) crates.push_back(s);
+      else {
+        int a, b, c;
+        sscanf(s.c_str(), "move %d from %d to %d", &a, &b, &c);
+        b--, c--;
+        ops.push_back({a, b, c});
+      }
+    }
+  }
+
+  for(auto op : ops) {
+    int a = op[0], b = op[1], c = op[2];
+    while(a--) {
+      crates[c].push_back(crates[b].back());
+      crates[b].pop_back();
+    }
+  }
+  string res;
+  for(auto c : crates) {
+    if(!c.empty()) res += c.back();
+  }
+  cout << res << endl;
+  return 0;
+}
